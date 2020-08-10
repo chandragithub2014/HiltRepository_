0f22b21d7297bb6e2a6f8373582708fb6df2e5be
Chandra <chandrasaimohan.bhupathi@colruytgroup.com>
2020-08-10 15:05:27 +0200

    updated with Diffutils and Swipe To Refresh List

--- app/src/main/java/com/hilt/demo/MainActivity.kt
+++ app/src/main/java/com/hilt/demo/MainActivity.kt
@@ -13,8 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_main)
         replaceFragmentWithNoHistory(EmployeeSwipeFragment(), R.id.container_fragment)
+        someEmptyFunc()
 
     }
+
+    private fun  someEmptyFunc(){
+        println("SonarQube Info")
+        println("http://androidhubb.blogspot.com/2019/04/sonarqube-android-how-to-use-sonarqube.html")
+        println("gradlew sonarqube  -Dsonar.host.url=http://localhost:9000/")
+        println("https://rules.sonarsource.com/kotlin")
+        println("http://localhost:9000/profiles")
+        println("Detekt......")
+        println("https://medium.com/@heri.sulis/integrating-kotlin-detekt-cli-on-android-gradle-task-a02ca0378dcc")
+        println("https://medium.com/@abdullahbalta/improving-kotlin-quality-fc59df802ba8")
+        println("gradlew check")
+    }
+
+    private fun empty(){
+
+    }
+
+
+    private fun anotherEmpty(){
+
+    }
+
+
 }
 
 
